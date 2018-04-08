050ed372b627e8c0bcce136fbe043dac812d0d62
Melinda Hardin <melindawad@gmail.com>
2018-04-08 17:26:23 -0500

    logging items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,10 @@
+//NPM MySQL package to link MySQL & NODE
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '#', //put in password, but will hide in .env
-  database : 'students'
+  database : 'bamazon'
 });
  
 connection.connect(function(err) {
@@ -13,5 +14,19 @@ connection.connect(function(err) {
     }
    
     console.log('connected as id ' + connection.threadId);
-
+    afterConnection();
 });
+
+//display all of the items available for sale. Include the ids, names, and prices of products for sale.
+function afterConnection(){
+ connection.query("SELECT * FROM products", function (err, results){
+    if (err)throw err;
+    for (var i = 0; i < results.length; i++){
+        console.log ("ID: " + results[i].item_id + "|","Product: " + results[i].product_name + "|", "Price: $" + results[i].price);
+    }
+
+    console.log (results.product_name);//gets product
+    connection.end();
+ });
+
+}
