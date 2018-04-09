c76c27d92de7255867f7400bfd9806047727f79b
Melinda Hardin <melindawad@gmail.com>
2018-04-08 23:41:40 -0500

    total??

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '#', //put in password, but will hide in .env
+    password: 'Tricia615', //put in password, but will hide in .env
     database: 'bamazon'
 });
 
@@ -14,7 +14,7 @@ connection.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
-    }    
+    }
     afterConnection();
 });
 
@@ -23,50 +23,49 @@ connection.connect(function (err) {
 function afterConnection() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
+        console.log(results);
         for (var i = 0; i < results.length; i++) {
             console.log("ID: " + results[i].item_id + "|", "Product: " + results[i].product_name + "|", "Price: $" + results[i].price);
-            
+            console.log("\n--------------------------------------\n");
+
         }
 
-        //constructor function to get user inputs
-        //prompt users with two messages.
-        //The first should ask them the ID of the product they would like to buy.
-        //The second message should ask how many units of the product they would like to buy.
-        function Buyer(buy, units) {
-            this.buy = buy;
-            this.units = units;
-        }
-    
+        //constructor function to get user inputs .
+        function Buyer(itemBuying, unitsPurchasing) {
+            this.itemBuying = itemBuying;
+            this.unitsPurchasing = unitsPurchasing;
+            //this.price= <----------HOW TO GET PRICE OF SELECTION????????
+            //this.inStock = __________; //<----------HOW TO GET STOCK OF SELECTION????????
+
+
+            this.orderFulfilled = function () {
+                var total = this.unitsPurchasing * results.price;
+                console.log("Thank you for your purchase. Your total comes to $" + total + ".");
+            };
+            this.orderNotFulfilled = function () {
+                console.log("Sorry, we only have " + inStock + "currently in stock.  Would you like to ")
+            };
+        };
+
+        //prompt users with two messages
         inquirer.prompt([
             {
-                name: "buy",
+                name: "itemBuying",
                 message: "What is the ID of the product you would like to purchse?"
             },
-    
+
             {
-                name: "units",
+                name: "unitsPurchasing",
                 message: "How many would you like to purchase?"
             }
         ]).then(function (answer) {
             var newBuyer = new Buyer(
-                answer.buy,
-                answer.units
+                answer.itemBuying,
+                answer.unitsPurchasing
             );
-    
-    
+            //IF ELSE statement here for orders info
+            newBuyer.orderFulfilled();
         });
+
     });
-
 }
-
-        //Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-            //IF (does have enough of the product, you should fulfill the customer's order.)
-
-
-                //This means updating the SQL database to reflect the remaining quantity.
-
-                //Once the update goes through, show the customer the total cost of their purchas
-
-            //ELSE } If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
